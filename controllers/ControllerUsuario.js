4e10dd266f58cd2f5ae906e5e3f2f94bb46d40cf
Neosphers0863 <estivenvalencia0863@gmail.com>
2024-11-27 17:56:06 -0500

    Simulador de creditos en proceso

---
+++ controllers/ControllerUsuario.js
@@ -0,0 +1,74 @@
+import { usuarios } from "../models/ModelUsuario.js";
+
+export function login() {
+  let loginUsuario = document.getElementById("login-usuario").value;
+  let loginContrasena = document.getElementById("login-contrasena").value;
+  console.log(usuarios);
+  usuarios.some(function (index) {
+    // console.log(index)
+    if (loginUsuario == index.usuario && loginContrasena == index.contrasena) {
+      let timerInterval;
+      Swal.fire({
+        title: "Bienvenido: " + index.nombre,
+        html: "Será redireccionado en: <b></b> milliseconds.",
+        timer: 4000,
+        icon: "success",
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading();
+          const timer = Swal.getPopup().querySelector("b");
+          timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+          }, 100);
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+          window.location.href = "/views/pages/viewCredito.html";
+        },
+      }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+          console.log("I was closed by the timer");
+        }
+      });
+    } else {
+      Swal.fire({
+        title: "Error",
+        text: "Usuario y/contraseña incorrecto o no existe",
+        icon: "error",
+      });
+    }
+  });
+}
+
+export function register() {
+  let registroNombre = document.getElementById("register-nombre").value;
+  let registroUsuario = document.getElementById("register-usuario").value;
+  let registroCorreo = document.getElementById("register-correo").value;
+  let registroContrasena = document.getElementById("register-contrasena").value;
+  let registroConfirmar = document.getElementById("register-confirmar").value;
+  let newRegistro = {
+    nombre: registroNombre,
+    usuario: registroUsuario,
+    correo: registroCorreo,
+    contrasena: registroContrasena,
+    confirmar: registroConfirmar,
+  };
+  usuarios.unshift(newRegistro);
+  document.getElementById("form-register").style.display = "none";
+  document.getElementById("form-login").style.display = "flex";
+  console.log(usuarios);
+}
+
+// let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(numeros)
+// numeros.pop()
+// console.log(numeros)
+// numeros.pop()
+// console.log(numeros)
+// numeros.shift()
+// console.log(numeros)
+// numeros.shift()
+// console.log(numeros)
+// numeros.shift()
+// console.log(numeros)
---
+++ views/public/js/script.js
@@ -0,0 +1,103 @@
+import { expresionesRegulares } from "../../../helpers/expresiones.js";
+
+// console.log(expresionesRegulares.nombre);
+
+document
+  .querySelector("#btnFormRegistro")
+  .addEventListener("click", function () {
+    document.querySelector("#form-login").style.display = "flex";
+    document.querySelector("#form-register").style.display = "none";
+  });
+document.querySelector("#btnFormLogin").addEventListener("click", function () {
+  document.querySelector("#form-login").style.display = "none";
+  document.querySelector("#form-register").style.display = "flex";
+});
+
+let inputs = document.getElementsByTagName("input");
+for (let index = 0; index < inputs.length; index++) {
+  inputs[index].addEventListener("keyup", validarFormulario);
+}
+function validarFormulario(e) {
+  //   console.log(e);
+  switch (e.target.name) {
+    case "register-nombre":
+      if (expresionesRegulares.nombre.test(e.target.value)) {
+        document.getElementById("register-nombre").classList.add("correcto");
+        document
+          .getElementById("register-nombre")
+          .classList.remove("incorrecto");
+        // document.getElementById("mensaje-nombre").textContent =
+        // "";
+      } else {
+        document.getElementById("register-nombre").classList.add("incorrecto");
+        document.getElementById("register-nombre").classList.remove("correcto");
+        // document.getElementById("mensaje-nombre").textContent =
+        //   "El nombre solo puede ir en mayúsculas";
+      }
+      break;
+    case "register-usuario":
+      if (expresionesRegulares.usuario.test(e.target.value)) {
+        document.getElementById("register-usuario").classList.add("correcto");
+        document
+          .getElementById("register-usuario")
+          .classList.remove("incorrecto");
+      } else {
+        document.getElementById("register-usuario").classList.add("incorrecto");
+        document
+          .getElementById("register-usuario")
+          .classList.remove("correcto");
+      }
+      break;
+    case "register-correo":
+      if (expresionesRegulares.correo.test(e.target.value)) {
+        document.getElementById("register-correo").classList.add("correcto");
+        document
+          .getElementById("register-correo")
+          .classList.remove("incorrecto");
+      } else {
+        document.getElementById("register-correo").classList.add("incorrecto");
+        document.getElementById("register-correo").classList.remove("correcto");
+      }
+      break;
+    case "register-contrasena":
+      if (expresionesRegulares.contraseña.test(e.target.value)) {
+        document
+          .getElementById("register-contrasena")
+          .classList.add("correcto");
+        document
+          .getElementById("register-contrasena")
+          .classList.remove("incorrecto");
+      } else {
+        document
+          .getElementById("register-contrasena")
+          .classList.add("incorrecto");
+        document
+          .getElementById("register-contrasena")
+          .classList.remove("correcto");
+          
+      }
+      break;
+    case "register-confirmar":
+      if (expresionesRegulares.contraseña.test(e.target.value)) {
+        document.getElementById("register-confirmar").classList.add("correcto");
+        document
+          .getElementById("register-confirmar")
+          .classList.remove("incorrecto");
+      } else {
+        document
+          .getElementById("register-confirmar")
+          .classList.add("incorrecto");
+        document
+          .getElementById("register-confirmar")
+          .classList.remove("correcto");
+      }
+      break;
+  }
+}
+
+// console.log(document.querySelectorAll('input'))
+
+// let arreglo = [1, 'jaime', true, function () { }, { nombre: 'jaime' }, []]
+// console.log(arreglo)
+// console.log(arreglo[4])
+// console.log(arreglo[2])
